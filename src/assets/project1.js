5549367e700dd0ce3d94d68f527970e5bea7666d
aaron <aaronlim314@gmail.com>
2021-11-11 16:05:36 +1100

    Initiated project information object files

---
+++ src/assets/project1.js
@@ -0,0 +1,28 @@
+const project1 = {
+    title: 'Japanese Sentence Searcher',
+    imgUrl: '#',
+    madeWith: [
+        {
+            text: 'HTML5',
+            icon: 'w3_html5'
+        },
+        {
+            text: 'CSS3',
+            icon: 'w3_css'
+        },
+        {
+            text: 'React.js',
+            icon: 'reactjs'
+        },
+        {
+            text: 'Sass',
+            icon: 'sass-lang'
+        },
+        {
+            text: 'Firebase',
+            icon: 'firebase'
+        },
+    ],
+}
+
+export { project1 };
---
+++ src/assets/project2.js
@@ -0,0 +1,32 @@
+const project2 = {
+    title: 'Food Ordering Website',
+    imgUrl: '#',
+    madeWith: [
+        {
+            text: 'HTML5',
+            icon: 'w3_html5'
+        },
+        {
+            text: 'CSS3',
+            icon: 'w3_css'
+        },
+        {
+            text: 'React.js',
+            icon: 'reactjs'
+        },
+        {
+            text: 'Sass',
+            icon: 'sass-lang'
+        },
+        {
+            text: 'Typescript',
+            icon: 'typescriptlang'
+        },
+        {
+            text: 'Firebase',
+            icon: 'firebase'
+        },
+    ],
+}
+
+export { project2 };
