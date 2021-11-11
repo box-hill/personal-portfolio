a6ebc5864e51f3acc16ab220df67e94fad0b61c9
aaron <aaronlim314@gmail.com>
2021-11-11 17:05:24 +1100

    Updated project info

--- src/assets/project-information.js
+++ src/assets/project-information.js
@@ -22,6 +22,10 @@ const project1 = {
             text: 'Firebase',
             icon: 'firebase'
         },
+        {
+            text: 'Google Search API',
+            icon: 'google'
+        },
     ],
 }
 
