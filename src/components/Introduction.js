47b2d27ac5b3ace8981ce4d20aae50cb462a0380
aaron <aaronlim314@gmail.com>
2021-11-12 17:10:56 +1100

    Updated Introduction information

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Introduction() {
+  const [ hidden, setHidden ] = useState(true);
+
   return (
-    <section>
-        <h1 className='slide-in-right'>
-        Bringing ideas to life is my passion
+    <section id='intro-section'>
+      <h1>
+        <span className='fade-in-delayed'>Hi! I'm <span id='first-name'>Aaron</span>.</span>
         <br/>
-        consectetur adipiscing elit. 
+        <span className='slide-in-right'>Bringing ideas</span>
+        <span className='slide-in-left'>to life</span>
         <br/>
-        Phasellus et lectus id diam dictum dictum nec in nibh.
-        </h1>
+        <span className='slide-in-delayed'>is my passion.</span>
+      </h1>
     </section>
   );
 }
